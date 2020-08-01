2c99dbf94325c447fd1ca9557d9a6232b82ee5b7
adontz <adontz@gmail.com>
2020-08-01 23:06:56 +0400

    Prepare for GitHub Pages publishing.
    Create README.md
    
    Update README.md
    
    Create LICENSE
    
    Create CNAME
    
    Add chapter pages.
    
    Add utteranc.es
    
    Fix navigation.
    
    Fix grammar.
    
    Update generated files.
    
    Add OpenAPI.
    Reformat roadmap.
    
    Split project into components.
    
    Recover explanations/about
    
    Improve build script.
    
    Fix logotype.
    
    Update UI
    
    Fix typo.
    
    Add About.
    
    Rename how-tos to how-to, add missing About files.
    
    Removed source files from this repository.
    
    Fix home page.
    
    Update sitemap.
    
    Add FreeFind search.
    
    Latest version.

--- build.cmd
+++
@@ -1,4 +0,0 @@
-antora generate --clean playbook.yml
-copy /y NUL .\build\site\.nojekyll
-echo "<!DOCTYPE html><html><head><meta http-equiv='refresh' content='0; url=/nt-rest/1.0/index.html' /></head><body></body></html>" > .\build\site\index.html 
-
